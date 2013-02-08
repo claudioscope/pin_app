313725ce34edfe8d37a87097dde24a5a6bb24931
claudiomiranda <claudioscope@gmail.com>
2013-02-08 00:39:51 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PinApp::Application.config.secret_token = 'b94e80b607bda63faf471e8eb60a981ee1d398d157cb1a1ac0d710729c61d9e894b492dfaac72756cbfcdba94e48b8e944d7a2e104c9280969ff18c167f3cba0'
